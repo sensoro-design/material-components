d90e1ef53ce3d0f31ad512cacacbf950ee47e39f
wangxingkang <wang_xingkang@qq.com>
2023-02-20 15:07:40 +0800

    feat(provider): add setAlpha、lighten utils

--- packages/provider/src/index.ts
+++ packages/provider/src/index.ts
@@ -2,5 +2,6 @@ export { MaterialProvider } from './ConfigContext';
 export { MaterialConfigProvider } from './ConfigProvider';
 export { useStyle } from './hooks/useStyle';
 export { resetComponent } from './utils/resetComponent';
+export { setAlpha, lighten } from './utils/color';
 
 export type { MaterialAliasToken, GenerateStyle, } from './types';
---
+++ packages/provider/src/utils/color.ts
@@ -0,0 +1,21 @@
+import { TinyColor } from '@ctrl/tinycolor';
+
+/**
+ * 把一个颜色设置一下透明度
+ * @example (#fff, 0.5) => rgba(255, 255, 255, 0.5)
+ * @param baseColor {string}
+ * @param alpha {0-1}
+ * @returns rgba {string}
+ */
+export const setAlpha = (baseColor: string, alpha: number) =>
+  new TinyColor(baseColor).setAlpha(alpha).toRgbString();
+
+/**
+ * 把一个颜色修改一些明度
+ * @example (#000, 50) => #808080
+ * @param baseColor {string}
+ * @param brightness {0-100}
+ * @returns hexColor {string}
+ */
+export const lighten = (baseColor: string, brightness: number) =>
+  new TinyColor(baseColor).lighten(brightness).toHexString();
