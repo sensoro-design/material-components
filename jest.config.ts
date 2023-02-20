3f306842d725b697c1cbf006ec048b01c080590b
wangxingkang <wang_xingkang@qq.com>
2023-02-20 12:09:29 +0800

    test: update config

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,18 @@
-import { createConfig } from '@umijs/test';
+import { createConfig, type Config } from '@umijs/test';
 
-const jestConfig = createConfig({
+const defaultConfig = createConfig({
   target: 'browser'
 });
 
-export default {
-  ...jestConfig
+const config: Config.InitialOptions = {
+  ...defaultConfig,
+  clearMocks: true,
+  resetMocks: false,
+  transform: {
+    ...defaultConfig.transform
+  },
+  setupFiles: [...(defaultConfig.setupFiles || [])],
+  setupFilesAfterEnv: [...(defaultConfig.setupFilesAfterEnv || []), './tests/setupFilesAfterEnv.ts']
 }
+
+export default config;
--- packages/components/src/battery/__tests__/index.test.ts
+++ packages/components/src/battery/__tests__/index.test.ts
@@ -1,3 +1,7 @@
-describe('Battery', () => {
+import { Battery } from '../';
 
+describe('Battery', () => {
+  it('should be defined', () => {
+    expect(Battery).toBeDefined();
+  });
 });
